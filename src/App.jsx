fea8ded9bd20da39fce8fc8b7e5874e8e24ade64
cristianvernal <cr.vernal@duocuc.cl>
2024-03-21 18:00:47 -0300

    Pagina inicio

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+import './App.css';
+import ButtonEnter from './components/button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div class="hero"> 
+  <div class="hero__title">Razas de Perros
+  </div> 
+  <div class="cube"></div>
+  <div class="cube"></div>
+  <div class="cube"></div>
+  <div class="cube"></div>
+  <div class="cube"></div>
+  <div class="cube"></div>
+<div className='boton'>
+  <ButtonEnter/>
+</div>
+</div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,17 @@
+import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
+function TypesExample() {
+ 
+ 
+ 
+ 
+    return (
+    <>
+      <Button variant="primary">Entrar</Button>{' '}
+      </>
+  );
+}
+
+export default TypesExample;
